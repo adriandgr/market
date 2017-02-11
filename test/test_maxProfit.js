0300db6b8880cfa12019c999c26a3c59ec85dd04
Adrian Diaz <web.art@adriandiaz.ca>
2017-02-11 05:28:03 +0000

    adds logic to maxProfit

--- test/test_maxProfit.js
+++ test/test_maxProfit.js
@@ -4,9 +4,17 @@ var assert = require('chai').assert;
 var maxProfit = require('../lib/maxProfit');
 
 describe('maxProfit()', function() {
-  it('should return a boolean', function() {
-    let good = 8532;
-    assert.typeOf(maxProfit(good), 'boolean');
+  it('should return a number', function() {
+    let data = [45, 24, 35, 31, 40, 38, 11];
+    assert.typeOf(maxProfit(data), 'number');
+  });
+  it('should return the maximum profit', function() {
+    let data = [45, 24, 35, 31, 40, 38, 11];
+    assert.equal(maxProfit(data), 16);
+  });
+  it('should retun \'-1\' if the array contains wrong type data', function() {
+    let data = [45, 24, 35, 31, '40y', 38, 11];
+    assert.equal(maxProfit(data), -1);
   });
 });
 
